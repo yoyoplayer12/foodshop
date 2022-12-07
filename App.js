36817d8461443cae12aa6093e8cf2f2e69cb154c
Yorick <yorick.devleeschouwer@gmail.com>
2022-12-07 11:27:42 +0100

    changed view to safeareaview for IOS

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, SafeAreaView } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
 
 
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
-      <Text>FoodShop is open!</Text>
-      <StatusBar style="StatusBar.setStatusBarHidden(hidden, animation)" />
+      <View>
+        <Text>FoodShop is open!</Text>
+        <StatusBar style="StatusBar.setStatusBarHidden(hidden, animation)" />
+      </View>
     </SafeAreaView>
   );
 }
